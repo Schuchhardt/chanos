4c79137830efb1d50373852ab7e19f9a0506fef7
Sebastián Schuchhardt <seba.sch999@gmail.com>
2018-08-22 15:15:49 -0300

    added firebase chano

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1>Chanos</h1>
+    <div class="gif">
+    </div>
+    <h1>Everywhere</h1>
   </div>
+    <!-- <router-view/> -->
 </template>
 
 <script>
@@ -17,7 +20,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.gif{
+  height: 100vh;
+  background-image: url('https://media3.giphy.com/media/K34FVrUx8ggyA/giphy.gif');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+@media(min-width: 767px){
+  .gif{
+    width: 50vw;
+    margin: auto;
+    display: block;
+  }
 }
 </style>
